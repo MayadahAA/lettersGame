7090b5d955698c88cf24c4d8b43c402c87649d1a
MayadahAA <97852885+MayadahAA@users.noreply.github.com>
2025-03-13 20:27:02 +0300

    remove socket.io

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { Server as NetServer } from 'http';
-import { Server as SocketIOServer } from 'socket.io';
-
 // التعامل مع اتصالات Socket.io واختراق طلبات WebSocket
 // هذا middleware مسؤول عن اعتراض طلبات Socket.io والتعامل معها بشكل مناسب
 
