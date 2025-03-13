9cf40f2a076454400c1387e8ab7b6038084047aa
MayadahAA <97852885+MayadahAA@users.noreply.github.com>
2025-03-13 20:16:37 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
-import dynamic from 'next/dynamic';
+// import Image from "next/image";
+// import dynamic from 'next/dynamic';
 
-const ClientComponent = dynamic(
-  () => import('./components/SocketClient'),
-  { ssr: false }
-);
+import React from "react";
+
+// const ClientComponent = dynamic(
+//   () => import('./components/SocketClient'),
+//   { ssr: false }
+// );
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      {/* <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -107,7 +109,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
